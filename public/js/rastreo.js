5d9b26c549527ca05a5dff66215d60cfb82d1970
Javier Hernandez <javierv31@gmail.com>
2023-01-09 16:21:10 -0600

    MEnu:guia, submenu:rastreo, Se agregan botones para excel

--- public/js/rastreo.js
+++ public/js/rastreo.js
@@ -42,6 +42,39 @@ function rastrearTabla(){
                     ,data: response.data
                     ,autoWidth: false
                     ,order: [[0, 'desc']]
+                    ,lengthMenu: [
+                        [ 10, 25, 50, -1 ],
+                        [ '10', '25', '50', 'Todo' ]
+                    ]
+                    ,dom: 'Bfrtip'
+                    ,buttons: [ 
+                        'pageLength'
+                      ,{ 
+                         extend: 'excelHtml5'
+                         , footer: true
+                         , charset: 'utf-8' 
+                         , fieldSeparator: ','
+                         ,fieldBoundary: ''
+                         ,exportOptions: {
+                            columns: ':not(.notexport)'
+                         }
+                         ,customizeData: function(data) {
+                           for(var i = 0; i < data.body.length; i++) {
+                            console.log("custom")
+                             data.body[i][2] = '\0' + data.body[i][2];
+                           }
+                         } 
+                      }
+                      ,{ 
+                         extend: 'pdf'
+                         ,orientation: 'landscape'
+                         , footer: true 
+                         ,exportOptions: {
+                            columns: ':not(.notexport)'
+                         } 
+                      }
+                      
+                   ]
                     ,columnDefs: [
                         {  
                             targets: 7 
