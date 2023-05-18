aab9ac1dfe0db483b6ee58e80955291956c50b82
Javier Hernandez <javierv31@gmail.com>
2023-05-17 23:47:01 -0600

    Menu:guia - submenu:creacion - Descripcion:Se agregan lineas apra al direccion

--- app/Dto/DhlDTO.php
+++ app/Dto/DhlDTO.php
@@ -67,15 +67,16 @@ class DhlDTO
         //Origen
         $addressLine1 = sprintf("%s %s %s",$request["direccion"], $request["no_int"], $request["no_ext"] );
 
+        $addressLine2 = sprintf("Col. %s",$request["colonia"]); 
+        $referencia = str_split($request["direccion2"],44)[0];
 
-        $addressLine2 = str_split($request["direccion2"],44)[0];
-
-        $addressLine2 = ( strlen($addressLine2) > 0 ) ? $addressLine2 : "Sin referencia" ;
+        $referencia = ( strlen($referencia) > 0 ) ? $referencia : "Sin referencia" ;
     	$postalAddress = new PostalAddress(
                 array("cityName"    => $request["ciudad"]
                     ,"postalCode"   => $request["cp"]
                     ,"addressLine1" => $addressLine1
-                    ,"addressLine2"  => $addressLine2
+                    ,"addressLine2" => $addressLine2
+                    ,"addressLine3"  => $referencia
                 )
             );
     	$contactInformation = new ContactInformation(
@@ -90,15 +91,17 @@ class DhlDTO
         //Destino
         $addressLine1 = sprintf("%s %s %s",$request["direccion_d"], $request["no_int_d"], $request["no_ext_d"] );
 
-        $addressLine2_d = str_split($request["direccion2_d"],44)[0];
+        $addressLine2_d = sprintf("Col. %s",$request["colonia_d"]);
+        $referencia_d = str_split($request["direccion2_d"],44)[0];
 
-        $addressLine2_d = ( strlen($addressLine2_d) > 0 ) ? $addressLine2_d : "Sin referencia" ;
+        $referencia_d = ( strlen($referencia_d) > 0 ) ? $referencia_d : "Sin referencia" ;
 
     	$postalAddress = new PostalAddress(
                 array("cityName"    => $request["ciudad_d"]
                     ,"postalCode"   => $request["cp_d"]
                     ,"addressLine1" => $addressLine1
                     ,"addressLine2"  =>$addressLine2_d
+                    ,"addressLine3"  => $referencia_d
                 )
             );
     	$contactInformation = new ContactInformation(
