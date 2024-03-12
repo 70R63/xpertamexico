2710de681f9b7688e2d2fd530552159c5724924e
Javier Hernandez <javierv31@gmail.com>
2024-03-11 21:26:59 -0600

    Menu-dashboard - Submenu:validacion de pago en linea - Descripcion: se valida las formas y alcances para uso de pasaeral mercado pago

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
 Route::resource('profile','userProfileController');
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard.index');
 })->middleware(['auth'])->name('dashboard');
 
 /*
